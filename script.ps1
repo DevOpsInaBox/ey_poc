afd01ebf7f069b4d019d2fd3f5c6962afed07f39
Devops <devopsInabox@newtglobal.com>
2017-07-12 12:32:58 +0530

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -1,1 +1,1 @@
-cmd.exe /c 'F:\SCOM\InstallSCOMAgent.bat'
+cmd.exe /c 'winrm create winrm/config/Listener?Address=*+Transport=HTTPS @{Hostname="localhost"; CertificateThumbprint="25E068D2D7600FBF92735225A1706D0F39958E3A"}'
